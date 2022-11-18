38e84d44c31a60a6eee47baf5ba424352c144e70
GroguA <117662071+GroguA@users.noreply.github.com>
2022-11-18 21:35:14 +0300

    task4 added

---
+++ task4/main.swift
@@ -0,0 +1,22 @@
+//
+//  main.swift
+//  task4
+//
+//  Created by Александра Сергеева on 18.11.2022.
+//
+
+import Foundation
+
+func multiplication() {
+    let numbers = [1, 2, 3, 4, 5]
+    for number in numbers {
+        for secondNumber in numbers {
+            var result = number * secondNumber
+            var resultForPrint = "\(number) * \(secondNumber) = \(result)"
+            print(resultForPrint)
+        }
+        print("")
+    }
+}
+
+multiplication()
