206674cadced24e92098cb2eaa758c17983374bc
GroguA <117662071+GroguA@users.noreply.github.com>
2022-11-21 13:54:49 +0300

    add task5

---
+++ task5/main.swift
@@ -0,0 +1,25 @@
+//
+//  main.swift
+//  task5
+//
+//  Created by Александра Сергеева on 21.11.2022.
+//
+
+import Foundation
+
+func simpleNumbersPrint() {
+    let numbers = 2...100
+    for number in numbers {
+        var isSimple = true
+        let dividers = 2..<number
+        for divider in dividers {
+            if number % divider == 0 {
+                isSimple = false
+            }
+        }
+        print("\(isSimple) \(number)")
+    }
+}
+
+simpleNumbersPrint()
+
