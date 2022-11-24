c3636bf0dbff4c65e8baca0e9a9b14e76888328b
GroguA <117662071+GroguA@users.noreply.github.com>
2022-11-24 17:45:51 +0300

    array from one number added

---
+++ task16/main.swift
@@ -0,0 +1,24 @@
+//
+//  main.swift
+//  task16
+//
+//  Created by Александра Сергеева on 24.11.2022.
+//
+
+import Foundation
+
+//Write a function that takes a number and returns a list of its digits. So for 2342 it should return [2,3,4,2].
+
+var newArray = [Int]()
+
+func newListFromInts(integer: Int) -> Array<Int> {
+    var number = integer
+    while number % 10 != 0 {
+        var ostatok = number % 10
+        newArray.append(ostatok)
+        number = number / 10
+    }
+    return newArray.reversed()
+}
+
+print(newListFromInts(integer: 132))
